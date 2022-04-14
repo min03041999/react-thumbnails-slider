0883a324129ec36a18d9efad77260f506ea4243b
DESKTOP-4GUMU4J\Amin <min03041999@gmail.com>
2022-04-14 21:52:44 +0700

    feat(source): add source

---
+++ src/components/product-images-slider/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./product-images-slider.scss";
+
+index.propTypes = {};
+
+function index(props) {
+  return <div>Index</div>;
+}
+
+export default index;
